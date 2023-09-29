f015b3146b1b6fe94af566c5c5b8cdb663398988
seikosilvakomesu <seikosilvakomesu@gmail.com>
2023-09-28 19:05:54 -0500

    Subiendo proyecto vacio

---
+++ src/main/java/org/cibertec/edu/pe/DAWI_CL1_SILVA_GABRIEL.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAWI_CL1_SILVA_GABRIEL {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DAWI_CL1_SILVA_GABRIEL.class, args);
+	}
+
+}
---
+++ src/test/java/org/cibertec/edu/pe/DAWI_CL1_SILVA_GABRIELTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAWI_CL1_SILVA_GABRIELTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
